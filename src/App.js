9b63462fb80df7a8031ff099010e0868ae0e1fb4
mageer <mageer.github@gmail.com>
2021-02-26 18:21:59 +0200

    testing PR

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This will fail.
+          This will fails.
         </a>
       </header>
     </div>
